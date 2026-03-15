a73a6326b06683483da06ad6b064be375f9c78f0
Elena Suslova <elenakovyazina1986@yandex.ru>
2026-03-15 06:15:19 +0100

    Добавлен новый файл

---
+++ best_movies.py
@@ -0,0 +1,32 @@
+  GNU nano 8.7                     best_movies.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                  [ New File ]
+^G Help      ^O Write Out ^F Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
+best_movies = ['Побег из Шоушенка', 'Криминальное чтиво', 'Форрест Гамп', 'Леон', 'Король Лев']
+for movie in best_movies:
+    print(movie)
+
+print('Я не ходил в киношколу — я ходил в кино.')
